45e9dff09309e9eb171c4c3fd1e4742a645b73d1
grey-matter <aravindkm95@gmail.com>
2021-03-21 23:27:29 +0800

    Feature Extraction

--- feaure_extractor.py
+++ feaure_extractor.py
@@ -2,37 +2,13 @@
 # Courtesy of https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html#initialize-and-reshape-the-networks
 
 from dataset import VRDataset
+from encoder_cnn import EncoderCNN
 import torch
 import torchvision
 from torchvision import transforms, datasets, models, utils
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 import numpy as np
-
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-class EncoderCNN(nn.Module):
-    def __init__(self, output_feature_dims = 500):
-        super(EncoderCNN, self).__init__()
-
-        self.model = models.resnet50(pretrained=True)
-
-        for param in model.parameters():
-            param.requires_grad = False
-
-        self.model.fc = nn.Linear(2048, output_feature_dims)
-
-        # self.dropout = nn.Dropout(p=0.5)
-
-        self.relu = nn.ReLU()
-
-    def forward(self, images):
-        return self.relu(self.model(images))
 
 output_feature_dims = 500
 input_size = 224
@@ -53,24 +29,10 @@ data_transformations = {
 	])
 }
 
-model = models.resnet50(pretrained=True)
-
-for param in model.parameters():
-	param.requires_grad = False
-
-model.fc = nn.Linear(2048, output_feature_dims)
-
-train_dataset = datasets.ImageFolder(train_dataset_path, transform=data_transformations['train'])
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=31, shuffle=False)
-
 vrdataset = VRDataset(img_root=train_dataset_path, transform=data_transformations['train'])
 dataloader = DataLoader(vrdataset, batch_size=1, shuffle=False, num_workers=0)
 
-sample = next(iter(dataloader))
-
-# imshow(torchvision.utils.make_grid(sample['frames'][20]))
-
 encoder = EncoderCNN()
 for i, sample in enumerate(dataloader):
-    r = encoder.forward(sample['frames'])
+    r = encoder.forward(sample['frames'][0])
     print(r.shape)
