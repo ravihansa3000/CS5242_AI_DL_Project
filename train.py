5ec690bb0691297bf85b3e072f3ffdf8bba994f2
Akila Perera <akila.perera@illumina.com>
2021-04-03 19:58:08 +0800

    Minor fixes - logging

--- train.py
+++ train.py
@@ -1,19 +1,18 @@
-import sys
 import json
-import os
-import time
-import subprocess
 import logging
+import os
+import sys
+import time
 
-import numpy as np
 import torch
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import transforms
+from torch.utils.data import DataLoader
 
+import utils
 from dataset import VRDataset
-from utils import save_checkpoint
 from model_config import model_options, model_provider, data_transformations
+from utils import save_checkpoint
 
 from optical_flow import *
 
@@ -25,62 +24,86 @@ logging.basicConfig(
 
 
 def train(dataloader, model, optimizer, lr_scheduler, opts):
-	loss_fns = [torch.nn.CrossEntropyLoss() for _ in range(3)]
+	loss_fns = [torch.nn.CrossEntropyLoss().to(device) for _ in range(3)]
 	with open(opts["train_annotation_path"]) as f:
-		training_annotation = json.load(f)
+		train_ann_dict = json.load(f)
 
 	logging.info(f"Starting training at {opts['start_epoch']} epochs and will run for {opts['end_epoch']} epochs "
 	             f"using device: {device}")
-	loss = None
+
 	for epoch in range(opts["start_epoch"], opts["end_epoch"]):
 		step = 0
 		true_pos = 0
 		total = 0
+		losses_str = ''
+		optimizer_lr_str = ''
+		mAPk_scores_str = 'N/A'
+
+		# disable teacher forcing after epoch milestone has elapsed
+		tf_mode = False if epoch > opts["disable_tf_after_epoch"] else True
+
 		for batch_idx, (video_ids, videos_tensor, videos_tensor_alternate) in enumerate(dataloader):
-			model.zero_grad()
+			# zero the parameter gradients
 			model.train()
+			optimizer.zero_grad()
 
 			videos_tensor = videos_tensor.to(device)
 			videos_tensor_alternate = videos_tensor_alternate.to(device)
+			# offset <relationship> annotations when generating word embeddings
+			batch_ann_t = torch.LongTensor([
+				[train_ann_dict[item][0], train_ann_dict[item][1] + 35, train_ann_dict[item][2]]
+				for item in video_ids
+			]).to(device)
+			output, _ = model(x=videos_tensor, x_alternate=videos_tensor_alternate, target_variable=batch_ann_t, tf_mode=tf_mode)
 
-			annots = torch.LongTensor([[training_annotation[item][0], training_annotation[item][1] + 35, training_annotation[item][2]]
-							 			for item in video_ids]).to(device)
-			output, _ = model(logging, x=videos_tensor, x_alternate=videos_tensor_alternate, target_variable=annots)
-			annots[:, 1] = torch.sub(annots[:, 1], 35)
-			loss = loss_fns[0](output[0], annots[:, 0]) + \
-			       loss_fns[1](output[1], annots[:, 1]) + \
-			       loss_fns[2](output[2], annots[:, 2])
+			# de-offset <relationship> annotations when calculating loss since linear output has 35 nodes
+			batch_ann_t[:, 1] = torch.sub(batch_ann_t[:, 1], 35)
+			loss1 = loss_fns[0](output[0], batch_ann_t[:, 0])
+			loss2 = loss_fns[1](output[1], batch_ann_t[:, 1])
+			loss3 = loss_fns[2](output[2], batch_ann_t[:, 2])
+			total_loss = loss1 + loss2 + loss3
+			total_loss.backward()
 
-			loss.backward()
-			torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+			# optimize with a gradient clip
+			torch.nn.utils.clip_grad_norm_(model.parameters(), opts["grad_clip"])
 			optimizer.step()
-			lr_scheduler.step()
 
-			lr_params = []
-			for param_group in optimizer.param_groups:
-				lr_params.append(param_group["lr"])
-			logging.info(f'lr_params: {lr_params}')
-			logging.info(f"Step update | batch_idx: {batch_idx}, step: {step}, loss: {loss.item()}")
+			# print stats for the step
+			losses_str = f"{total_loss:.6f} :: {loss1:.6f}, {loss2:.6f}, {loss3:.6f}"
+			optimizer_lr_str = f'{optimizer.param_groups[0]["lr"]:.3f}'
 
 			with torch.no_grad():
-				preds = [torch.nn.functional.log_softmax(op, dim=1) for op in output]
+				preds = [F.log_softmax(op, dim=1) for op in output]
 				true_pos_per_step = 0
 				total_per_step = opts["batch_size"]
 				preds = torch.stack([torch.argmax(pred, dim=1) for pred in preds], dim=1)
-				for (pred, annot) in zip(preds, annots):
-					logging.info(f"{pred}, {annot}")
+				for (pred, annot) in zip(preds, batch_ann_t):
 					if torch.equal(pred, annot):
 						true_pos_per_step += 1
 				true_pos += true_pos_per_step
 				total += total_per_step
 				step_acc = true_pos_per_step / total_per_step * 100
-				logging.info(f'Accuracy at step {step}: {step_acc}')
 
+			logging.info(f"Step update | epoch: {epoch}, batch_idx: {batch_idx}, step: {step}, "
+			             f"loss: {losses_str}, step_acc: {step_acc} | optimizer_lr: {optimizer_lr_str}")
 			step += 1
+
+		# calculate per epoch
+		lr_scheduler.step()
 		epoch_acc = true_pos / total * 100
-		logging.info(f'Accuracy at epoch {epoch}: {epoch_acc}')
-			
-		logging.info(f"Epoch update | epoch: {epoch}, loss: {loss.item()}")
+
+		# calculate overall MAP@k
+		if epoch % opts["mAP_k_print_interval"] == 0:
+			obj1_score, rel_score, obj2_score = utils.calculate_training_mAPk(
+				dataloader, model, train_ann_dict, opts)
+			mAPk_scores_str = f'{obj1_score:.3f}, {rel_score:.3f}, {obj2_score:.3f}'
+
+		# write epoch status update to file
+		model_info_path = os.path.join(opts["checkpoint_path"], 'score.txt')
+		with open(model_info_path, 'a') as fh:
+			time_str = time.strftime("%Y-%m-%d %H:%M:%S")
+			fh.write(f'{time_str} | epoch: {epoch}, loss: {losses_str}, optimizer_lr: {optimizer_lr_str}, '
+			         f'accuracy: {epoch_acc:.3f}, mAP@k: {mAPk_scores_str} \n')
 
 		if epoch % opts["save_checkpoint_every"] == 0:
 			save_file_path = os.path.join(opts["checkpoint_path"], f"model_{epoch}.pth")
@@ -91,12 +114,9 @@ def train(dataloader, model, optimizer, lr_scheduler, opts):
 			}, filename=save_file_path)
 			logging.info(f"Model saved to {save_file_path}")
 
-			model_info_path = os.path.join(opts["checkpoint_path"], 'score.txt')
-			with open(model_info_path, 'a') as fh:
-				time_str = time.strftime("%Y-%m-%d %H:%M:%S")
-				fh.write(f"{time_str} | model update --- epoch: {epoch}, loss: {loss:.6f} \n\n")
-	
-	logging.info("Training completed")
+		logging.info(f'Epoch update | epoch: {epoch}, loss: {losses_str}, optimizer_lr: {optimizer_lr_str}, '
+		             f'accuracy: {epoch_acc:.3f}, mAP@k: {mAPk_scores_str}')
+
 	return model
 
 
@@ -106,44 +126,37 @@ def main(opts):
 	if opts["resume"]:
 		if os.path.isfile(opts["resume"]):
 			logging.info(f'loading checkpoint {opts["resume"]}')
-			checkpoint = torch.load(args.resume)
+			checkpoint = torch.load(opts["resume"])
 			model.load_state_dict(checkpoint['state_dict'])
 			logging.info(f'loaded checkpoint {opts["resume"]}')
 		else:
-			logging.info(f'no checkpoint found at {opts["resume"]}')
+			raise RuntimeError(f'no checkpoint found at {opts["resume"]}')
+
 	logging.info(model)
 	optimizer = optim.Adam(model.parameters(), lr=opts["learning_rate"], weight_decay=opts["weight_decay"])
 	exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=opts["learning_rate_decay_every"],
 	                                             gamma=opts["learning_rate_decay_rate"])
-	vrdataset = VRDataset(img_root=opts["train_dataset_path"], img_root_alternate=os.path.join(opts['optical_flow_train_dataset_path'], opts['optical_flow_type']), len=opts['train_dataset_len'], transform=data_transformations(opts, mode='train'), transform_alternate=data_transformations(opts, mode='default'))
+	vrdataset = VRDataset(img_root=opts["train_dataset_path"], img_root_alternate=os.path.join(opts['optical_flow_train_dataset_path'], opts['optical_flow_type']), len=opts["train_dataset_size"],
+	                      transform=data_transformations(opts, mode='train'), transform_alternate=data_transformations(opts, mode='default'))
 	dataloader = DataLoader(vrdataset, batch_size=opts["batch_size"], shuffle=opts["shuffle"],
 	                        num_workers=opts["num_workers"])
+
 	train(dataloader, model, optimizer, exp_lr_scheduler, opts)
+	logging.info("Training completed")
 
 
 if __name__ == '__main__':
-	opts = vars(model_options())
-	opts["date"] = time.strftime("%Y-%m-%d %H:%M:%S")
-	opt_json = os.path.join(opts["checkpoint_path"], 'opt_info.json')
-	if not os.path.isdir(opts["checkpoint_path"]):
-		os.mkdir(opts["checkpoint_path"])
+	cli_opts = vars(model_options())
+	cli_opts["date"] = time.strftime("%Y-%m-%d %H:%M:%S")
+	opt_json = os.path.join(cli_opts["checkpoint_path"], 'opt_info.json')
+	if not os.path.isdir(cli_opts["checkpoint_path"]):
+		os.mkdir(cli_opts["checkpoint_path"])
 
-	logging.info(json.dumps(opts, indent=4))
+	logging.info(json.dumps(cli_opts, indent=4))
 	logging.info(f'__Python VERSION: {sys.version}')
 	logging.info(f'__pyTorch VERSION: {torch.__version__}')
 	with open(opt_json, 'w') as fh:
-		json.dump(opts, fh, indent=4)
+		json.dump(cli_opts, fh, indent=4)
 
-	# https://discuss.pytorch.org/t/cuda-visible-devices-make-gpu-disappear/21439
-	os.environ['CUDA_VISIBLE_DEVICES'] = opts["gpu"]
-	if opts["gpu"]:
-		logging.info(f'__CUDNN VERSION: {torch.backends.cudnn.version()}')
-		logging.info(f'__Number CUDA Devices: {torch.cuda.device_count()}')
-		cuda_result = subprocess.call(
-			["nvidia-smi", "--format=csv",
-			 "--query-gpu=index,name,driver_version,memory.total,memory.used,memory.free"])
-		logging.info(f'Available devices {torch.cuda.device_count()}')
-		logging.info(f'Current CUDA Device: GPU {torch.cuda.current_device()}')
-		logging.info(f'Current CUDA Device Name: {torch.cuda.get_device_name(int(opts["gpu"]))}')
-
-	main(opts)
+	utils.print_device(cli_opts)
+	main(cli_opts)
