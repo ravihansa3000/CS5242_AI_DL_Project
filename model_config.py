7e4513bb40a9279747d7af9b9be6a49e979b9992
Akila Perera <akila.perera@illumina.com>
2021-04-10 23:14:04 +0800

    Minor optimizations 2

--- model_config.py
+++ model_config.py
@@ -21,9 +21,9 @@ def model_options():
 	parser.add_argument('--rnn_dropout_p', type=float, default=0.5, help='strength of dropout for RNN layers')
 
 	parser.add_argument('--dim_hidden', type=int, default=500, help='size of the rnn hidden layer')
-	parser.add_argument('--dim_word', type=int, default=800, help='the encoding size of each token in the vocabulary')
-	parser.add_argument('--dim_vid', type=int, default=400, help='dim of features of video frames')
-	parser.add_argument('--dim_opf', type=int, default=400, help='dim of features of optical flow frames')
+	parser.add_argument('--dim_word', type=int, default=1000, help='the encoding size of each token in the vocabulary')
+	parser.add_argument('--dim_vid', type=int, default=500, help='dim of features of video frames')
+	parser.add_argument('--dim_opf', type=int, default=500, help='dim of features of optical flow frames')
 	parser.add_argument('--vocab_size', type=int, default=117 + 1, help='vocabulary size')
 
 	parser.add_argument('--learning_rate', type=float, default=1e-3, help='learning rate')
