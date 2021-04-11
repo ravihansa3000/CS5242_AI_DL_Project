0f7d308d115171051361b1e193452f0b732832b2
Aravind <aravindkm95@gmail.com>
2021-04-11 20:51:33 +0800

    Changes corresponding to highest score (#23)
    * adding optical flow
    
    * BiLSTM, bug fixing (#18)
    
    * Offset relationship by 35
    
    * Separate nn.Linear for each timestep
    
    * Add to device, changes in train.py
    
    * Take batch_size from opts
    
    * Step accuracy fix
    
    * Logging
    
    * Bug fix
    
    * Debugging
    
    * Pass via cl args
    
    * Debugging
    
    * Using resnet18
    
    * Changes
    
    * Debugging
    
    * Clipping
    
    * Some changes
    
    * Minor changes
    
    * Replace with VGG-11
    
    * BiRNN as an encoder, random noise injection
    
    * Remove logging
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * Minor fixes - logging
    
    * Refactored optical flow
    Changed to probabilistic TF
    Fixed conda env yml
    
    * add conda env yml for osx
    
    * Add dropout layers for RNN output
    Switch to dense_hsv method for optical flow generation
    Minor improvements
    
    * Add dropout for RNN input
    More linear + dropout layers in VGG classifiers
    
    * Add evaluation results
    
    * Minor improvements
    
    * Add I3D model
    
    * Bug fixes
    
    * Remove 2nd RNN input dropout
    Add i3d
    
    * Minor fixes
    
    * Use single LSTM (without padding)
    
    * Image Augmentation
    
    * fix lucas kanade optical flow issue
    
    * Minor optimizations
    
    Co-authored-by: lim0721lim <lim0721lim@hotmail.com>
    Co-authored-by: Akila Perera <akila.perera@illumina.com>

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,6 @@
 
 stdout_file=./model_run_data/test.out
 printf "Running test.py \n"
-nohup python3 ./test.py --gpu=0 --batch_size 40 $* > $stdout_file 2>&1 &
+nohup python3 ./test.py --gpu=0 --batch_size 60 $* > $stdout_file 2>&1 &
 
 printf "stdout: $stdout_file \n"
